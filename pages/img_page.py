d4678a4390c769c19f47fa907b991f8f2a46758c
Gabor Zorad EPAM <108816298+gabor-zorad-epam@users.noreply.github.com>
2023-05-16 17:50:38 +0200

    Finishing image tests.

--- pages/img_page.py
+++ pages/img_page.py
@@ -18,6 +18,7 @@ class DuckDuckGoImageResultPage:
     # Locators
     IMAGES_TAB = (By.CSS_SELECTOR, 'a[data-zci-link="images"]')
     IMAGE_4 = (By.CSS_SELECTOR, ':nth-child(4 of div.tile)')
+    IMAGE_TITLE = (By.CSS_SELECTOR, ':nth-child(4 of div.tile) > a > .tile--img__title')
     CLOSE_ICON = (By.CLASS_NAME, 'detail__close')
 
     # Initializer
@@ -25,15 +26,22 @@ class DuckDuckGoImageResultPage:
         self.browser = browser
 
     # Interaction Methods:
-    def img_search_result(self):
+    def img_search_result(self, phrase):
         # Click on the images Tab, a: contains text "Images"
         images_tab = self.browser.find_element(*self.IMAGES_TAB)
         images_tab.send_keys(Keys.RETURN)
 
+        # Check if the images Tab has the is-active class
+        cls_list = images_tab.get_attribute('class')
+        assert 'is-active' in cls_list
+
         # Select the 4th image class: tile--img
         image_4 = self.browser.find_element(*self.IMAGE_4)
-        # Click on the 4th image
         image_4.click()
+
+        # Check if the image title contains the Phrase
+        img_title = self.browser.find_element(*self.IMAGE_TITLE).get_attribute('innerHTML').lower()
+        assert phrase.lower() in img_title
 
         # Click on the X icon - class: js-detail-close or detail__close
         close_icon = self.browser.find_element(*self.CLOSE_ICON)
--- tests/test_search_image.py
+++ tests/test_search_image.py
@@ -1,16 +1,23 @@
 import time
-
-from pages.search_page import DuckDuckGoSearchPage
-from pages.result_page import DuckDuckGoResultPage
-from pages.result_page import DuckDuckGoImageResultPage
 import pytest
+from pages.search_page import DuckDuckGoSearchPage
+from pages.result_page import DuckDuckGoImageResultPage
 
 
 def test_duckduckgo_image_search(browser):
+    # Todo - Class initiation until the search should be moved maybe to the conftest.py file?
+    # Class initiation
+    search_page = DuckDuckGoSearchPage(browser)
+
+    # PHRASES to search for - Update to parametrize later!
+    PHRASE = 'panda'
+
+    # GIVEN The DuckDuckGo page is displayed
+    search_page.load()
+
+    # WHEN the user searches for a single "phrase"
+    search_page.search_single_phrase(PHRASE)
+
     # click on the Image section
     img_result = DuckDuckGoImageResultPage(browser)
-    img_result.img_search_result()
-
-    # - Assert / Verify if the Images tab is selected / currently active
-    # Check if the tab has the is-active class
-    # assert
+    img_result.img_search_result(PHRASE)
