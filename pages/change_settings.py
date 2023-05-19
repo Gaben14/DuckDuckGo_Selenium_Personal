504f05bdce06bf7fd7b187807f1be8cb3bb18277
Gabor Zorad EPAM <108816298+gabor-zorad-epam@users.noreply.github.com>
2023-05-19 14:17:39 +0200

    Assert completed for click_on_reset_btn method. Next Step is to refactor it.

--- pages/change_settings.py
+++ pages/change_settings.py
@@ -57,9 +57,9 @@ class DuckDuckGoSettings:
         )
         settings_a.click()
         # Turn on "Infinite Scroll" - setting_kav
-        #self.flipper_switch('kav')
+        self.flipper_switch('kav')
         # Turn on "Open Links in a New Tab" - setting_kn
-        #self.flipper_switch('kn')
+        self.flipper_switch('kn')
         # Click on Reset buttons.
         self.click_on_reset_btn()
         # Change region
@@ -81,7 +81,7 @@ class DuckDuckGoSettings:
 
         # Create a local variable in which you contain the parent's CSS selector 'class="frm__field  fix"'
         container_div = flipper.find_element(By.XPATH, '../..')
-        # TODO - Assert / Verify if the Parent div with 'class="frm__field  fix"' has the is-checked class
+        # Assert / Verify if the Parent div with 'class="frm__field  fix"' has the is-checked class
         container_div_cls_list = container_div.get_attribute('class')
 
         if self.is_checked in container_div_cls_list:
@@ -98,9 +98,8 @@ class DuckDuckGoSettings:
         sel_option_value = sel_option.get_attribute('value')
         sel_option.click()
 
-        # TODO -  Assert that value has actually changed
+        # Assert / Verify that value has actually changed
         assert sel_option_value == select_val
-
 
     def click_on_reset_btn(self):
         # Click on Reset inside the class: .settings-dropdown--header
@@ -110,9 +109,39 @@ class DuckDuckGoSettings:
             btn.click()
 
         # TODO - Assert if values are back to normal
+        # Create the values - Open the menu for the second check
+        settings_a = self.browser.find_element(*self.SETTINGS)
+        settings_a.click()
+
+        light_theme = self.browser.find_element(By.CSS_SELECTOR, 'div[data-theme-id="-1"] > label.set-theme')
+        light_theme_cls_list = light_theme.get_attribute('class')
+        assert self.is_checked in light_theme_cls_list
+
+        site_icons = self.browser.find_element(By.CSS_SELECTOR, 'label[for="setting_kf"')
+        site_icons_cont_div = site_icons.find_element(By.XPATH, "../..")
+        site_icons_cont_div_cls_list = site_icons_cont_div.get_attribute('class')
+        assert self.is_checked in site_icons_cont_div_cls_list
+
+        font_size = self.browser.find_element(By.CSS_SELECTOR, f'#setting_ks > option[value="n"]')
+        assert font_size.get_attribute('value') == 'n'
+
+        font_fam = self.browser.find_element(By.CSS_SELECTOR, f'#setting_kt > option[value="p"]')
+        assert font_fam.get_attribute('value') == 'p'
+
+        language = self.browser.find_element(By.CSS_SELECTOR, f'#setting_kad > option[value="wt_WT"]')
+        assert language.get_attribute('value') == 'wt_WT'
+
+        inf_scroll = self.browser.find_element(By.CSS_SELECTOR, 'label[for="setting_kav"')
+        inf_scroll_cont_div = inf_scroll.find_element(By.XPATH, '../..')
+        inf_scroll_cont_div_cls_list = inf_scroll_cont_div.get_attribute('class')
+        assert self.is_checked not in inf_scroll_cont_div_cls_list
+
+        open_new_links = self.browser.find_element(By.CSS_SELECTOR, 'label[for="setting_kn"')
+        open_new_links_cont_div = open_new_links.find_element(By.XPATH, '../..')
+        open_new_links_cont_div_cls_list = open_new_links_cont_div.get_attribute('class')
+        assert self.is_checked not in open_new_links_cont_div_cls_list
 
     # Assert For later: Save the values of these settings change, refresh the page.
-    # Check if the values are the same ?
     def change_region(self, country):
         region_btn = self.browser.find_element(By.CSS_SELECTOR, '.dropdown--region > a')
         region_btn.click()
