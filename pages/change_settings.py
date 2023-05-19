64a694f5d0bd990dfd0dab4003edc7fd7e7f41bf
Gabor Zorad EPAM <108816298+gabor-zorad-epam@users.noreply.github.com>
2023-05-19 12:42:30 +0200

    Assert completed for dropdown_change method.

--- pages/change_settings.py
+++ pages/change_settings.py
@@ -15,10 +15,12 @@ from selenium.webdriver.support import expected_conditions as EC
 class DuckDuckGoSettings:
     # Locators:
     SETTINGS = (By.CSS_SELECTOR, 'div.dropdown > a.zcm__link')
-    DARK_MODE = (By.CSS_SELECTOR, 'div[data-theme-id="d"]')
+    DARK_MODE = (By.CSS_SELECTOR, 'div[data-theme-id="d"] > label.set-theme')
 
     FONT_SIZE_SEL = 'setting_ks'
     FONT_FAMILY_SEL = 'setting_kt'
+
+    is_checked = 'is-checked'
     """
     The problem here is that we need to close the autocomplete result modal
     so that we can click on settings
@@ -28,6 +30,8 @@ class DuckDuckGoSettings:
     def __init__(self, browser: WebDriver):
         self.browser = browser
 
+        # Move all the method calls inside duck_settings to the constructor __init__
+
     # Interaction Methods:
     def duck_settings(self, font_size_val, font_fam_val, lang_val, country_val):
         # Click / Open on the Settings <a>
@@ -35,14 +39,9 @@ class DuckDuckGoSettings:
         settings_a = self.browser.find_element(*self.SETTINGS)
         settings_a.click()
 
-        # Change to Dark Mode
-        dark_mode = self.browser.find_element(*self.DARK_MODE)
-        dark_mode.click()
+        self.dark_mode()
 
-        # Assert / Verify if the Dark Mode has the is-checked class
-        dark_mode_cls_list = dark_mode.get_attribute('class')
-        assert 'is-checked' in dark_mode_cls_list
-
+        self.flipper_switch('kaz')
         # Trigger the "Site Icons" on and off - kf is for Site Icons
         self.flipper_switch('kf')
         # Font changes function call
@@ -58,34 +57,50 @@ class DuckDuckGoSettings:
         )
         settings_a.click()
         # Turn on "Infinite Scroll" - setting_kav
-        self.flipper_switch('kav')
+        #self.flipper_switch('kav')
         # Turn on "Open Links in a New Tab" - setting_kn
-        self.flipper_switch('kn')
+        #self.flipper_switch('kn')
         # Click on Reset buttons.
         self.click_on_reset_btn()
         # Change region
         self.change_region(country_val)
 
+    def dark_mode(self):
+        # Change to Dark Mode
+        dark_mode = self.browser.find_element(*self.DARK_MODE)
+        dark_mode.click()
+
+        # Assert / Verify if the Dark Mode has the is-checked class
+        dark_mode_cls_list = dark_mode.get_attribute('class')
+        assert self.is_checked in dark_mode_cls_list
+
     def flipper_switch(self, flipper_val):
+
         flipper = self.browser.find_element(By.CSS_SELECTOR, f'label[for="setting_{flipper_val}"]')
+        flipper.click()
 
         # Create a local variable in which you contain the parent's CSS selector 'class="frm__field  fix"'
-
+        container_div = flipper.find_element(By.XPATH, '../..')
         # TODO - Assert / Verify if the Parent div with 'class="frm__field  fix"' has the is-checked class
+        container_div_cls_list = container_div.get_attribute('class')
 
-        flipper.click()
+        if self.is_checked in container_div_cls_list:
+            assert self.is_checked in container_div_cls_list
+        else:
+            assert self.is_checked not in container_div_cls_list
 
     def dropdown_change(self, select_container, select_val):
         # Change the Font Size or Font-Family depending on the parameter
-        font_sel = self.browser.find_element(By.ID, select_container)
-        font_sel.click()
+        sel_container = self.browser.find_element(By.ID, select_container)
+        sel_container.click()
 
-        # TODO - Assert that value has actually changed
-
-        font_item = self.browser.find_element(By.CSS_SELECTOR, f'#{select_container} > option[value="{select_val}"]')
-        font_item.click()
+        sel_option = self.browser.find_element(By.CSS_SELECTOR, f'#{select_container} > option[value="{select_val}"]')
+        sel_option_value = sel_option.get_attribute('value')
+        sel_option.click()
 
         # TODO -  Assert that value has actually changed
+        assert sel_option_value == select_val
+
 
     def click_on_reset_btn(self):
         # Click on Reset inside the class: .settings-dropdown--header
