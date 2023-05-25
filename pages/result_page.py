18d0e526ad98e0e7eee8d8d685f56b93d890aeb4
Gabor Zorad EPAM <108816298+gabor-zorad-epam@users.noreply.github.com>
2023-05-25 15:51:16 +0200

    Refactor the Results Class to BDD

--- pages/result_page.py
+++ pages/result_page.py
@@ -3,6 +3,8 @@ This module contains the DuckDuckGoSearch (https://duckduckgo.com)
 page result call.
 """
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class DuckDuckGoResultPage:
@@ -14,17 +16,15 @@ class DuckDuckGoResultPage:
     AUTO_COMPLETE_RES = (By.CLASS_NAME, 'acp')
 
     # Initializer - using the browser fixture from the conftest.py
-    def __init__(self, browser, phrase):
+    def __init__(self, browser):
         self.browser = browser
-        self.phrase = phrase
 
     # Interaction Methods:
-    def search_input_value(self):
-        # WHEN
+    def then_search_input_value(self, phrase):
         search_input = self.browser.find_element(*self.SEARCH_INPUT)
         value = search_input.get_attribute('value')
 
-        assert self.phrase == value
+        assert phrase == value
 
     def click_on_search_result(self):
         # Select the first result, click on it
@@ -39,21 +39,21 @@ class DuckDuckGoResultPage:
         more_results_btn = self.browser.find_element(*self.MORE_RESULTS)
         more_results_btn.click()
 
-    def verify_autocomplete(self):
+    def then_verify_autocomplete(self):
         # Click inside the search input field - Here I'm already crossing against DRY, as I'm repeating the selector
-        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        # Add explicit wait
+        search_input = WebDriverWait(self.browser, 5).until(
+            EC.element_to_be_clickable(self.SEARCH_INPUT)
+        )
         search_input.click()
+
         search_input_val = self.browser.find_element(*self.SEARCH_INPUT).get_attribute("innerHTML")
         # Search for all the spans with the class 't-normal'
         autocomp_results = self.browser.find_elements(*self.AUTO_COMPLETE_RES)
         # Check if the search_input_value can be found inside the spans
 
         for result in autocomp_results:
-            # assert self.search_input_value() == result.get_attribute('innerHTML')
-            if search_input_val in result.get_attribute("innerHTML"):
-                return True
-            else:
-                return False
+            assert search_input_val in result.get_attribute("innerHTML")
 
     def select_autocomplete_selection(self):
         # Click inside the search input field - Here I'm already crossing against DRY, as I'm repeating the selector
@@ -65,20 +65,3 @@ class DuckDuckGoResultPage:
         third_result.click()
         # Go back to the page
         self.browser.back()
-
-    # Handler function
-    def search_result(self):
-        # THEN the search result links equals to "panda"
-        self.search_input_value()
-
-        # AND click on the first result
-        self.click_on_search_result()
-
-        # AND verify (assert) that the autocomplete results contain the phrase
-        assert self.verify_autocomplete()
-
-        # AND select an autocomplete result
-        self.select_autocomplete_selection()
-
-        # AND click on the More Results button
-        self.expand_more_result()
--- tests/test_search_result.py
+++ tests/test_search_result.py
@@ -1,8 +1,21 @@
 from pages.result_page import DuckDuckGoResultPage
-import pytest
 
 
 def test_basic_duckduckgo_search(browser, open_page, phrase):
     # Open the Search Result age
-    result_page = DuckDuckGoResultPage(browser, phrase)
-    result_page.search_result()
+    result_page = DuckDuckGoResultPage(browser)
+
+    # THEN assert / verify that the user has entered the right value
+    result_page.then_search_input_value(phrase)
+
+    # AND click on the first search result
+    result_page.click_on_search_result()
+
+    # THEN assert / verify if the auto complete suggestions contain the phrase
+    result_page.then_verify_autocomplete()
+
+    # AND click on the Auto Complete Selection
+    result_page.select_autocomplete_selection()
+
+    # AND click on the More Results button
+    result_page.expand_more_result()
